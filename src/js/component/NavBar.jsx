3d47bd76e5df75da36671fa5842ac65173db241f
Emily Vong <emily.vong25@gmail.com>
2018-07-16 14:31:19 +0000

    navbar with collapse

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -7,6 +7,39 @@ import PropTypes from 'prop-types';
 function NavBar(props){
     return (
         <div>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-secondary navbar-toggleable-md">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleContainer">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarToggleContainer">
+                    <a className="navbar-brand d-none d-sm-inline-block" href="#">
+                        <img src="http://via.placeholder.com/55x55" alt="logo" className="rounded-circle"/>
+                    </a>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Blog</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Store</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <button className="btn btn-outline-light">Account</button>
+                </div>
+            </nav>
+            <header className="bg-light p-3">
+                <img src="http://via.placeholder.com/80x80" alt="logo" className="rounded-circle mr-2"/>
+                <img src="http://via.placeholder.com/200x80" alt="website name" className="ml-2" />
+            </header>
+        </div>
+        
+        /*Previous NavBar code w/o toggle
+        <div>
             <nav className="navbar navbar-expand-sm navbar-dark bg-secondary">
                 <a className="navbar-brand" href="#">
                     <img src="http://via.placeholder.com/55x55" alt="logo" className="rounded-circle"/>
@@ -31,7 +64,7 @@ function NavBar(props){
                 <img src="http://via.placeholder.com/80x80" alt="logo" className="rounded-circle mr-2"/>
                 <img src="http://via.placeholder.com/200x80" alt="website name" className="ml-2" />
             </header>
-        </div>
+        </div>*/
     );
 }
 
