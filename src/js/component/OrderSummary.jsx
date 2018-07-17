73d20008240b5a24d22fb6714bbac8697e118821
KeShaunda <parrish309@gmail.com>
2018-07-17 20:19:28 +0000

    updated confirmation page and storehome

--- src/js/component/OrderSummary.jsx
+++ src/js/component/OrderSummary.jsx
@@ -8,16 +8,17 @@ import PropTypes from 'prop-types';
 function OrderSummary(props){
     return (
         
-        <div className="card">
+        <div className="card pt-4">
             <div className="card-body">
                 <h2 className="card-title">Order Summary</h2>
-                <a href="https://placeholder.com"><img src="https://via.placeholder.com/140x100"></img></a>
-                <p className="card-text">Qty: 1X $10.00</p>
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/150x100"></img></a>
+                <hr></hr>
+                <p className="card-text"><strong> Qty: 1</strong></p>
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">Subtotal: $10.00 </li>
-                <li className="list-group-item">Shipping: Free </li>
-                <li className="list-group-item">Sales Tax: $0.73 </li>
+                <li className="list-group-item"><strong>Subtotal:</strong> $10.00</li>
+                <li className="list-group-item"><strong>Shipping:</strong> Free </li>
+                <li className="list-group-item"><strong>Sales Tax:</strong> $0.73 </li>
             </ul>
             <div className="card-body">
                 <h3 className="card-title">Order Total: $10.73 </h3>
