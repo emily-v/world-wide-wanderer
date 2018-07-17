9a43c3161c0f92f86af33cf2126b23447dd38ac8
Emily Vong <emily.vong25@gmail.com>
2018-07-17 20:09:20 +0000

    completed blog post view and sidebar with dummy content

--- src/js/component/BlogSidebar.jsx
+++ src/js/component/BlogSidebar.jsx
@@ -5,7 +5,37 @@ import PropTypes from 'prop-types';
 function BlogSidebar(props){
     return (
         <div>
-            <h4 className="text-center">Sidebar Content</h4>
+            <div className="p-3 text-center sidebarContentContainer">
+                <img src="http://via.placeholder.com/150x200" alt="about-photo" className="img-fluid rounded-circle mt-5 mb-3"/>
+                <p><span className="font-weight-bold">Hello! </span>Raclette synth taiyaki pok pok coloring book selfies williamsburg godard. Aesthetic organic microdosing bespoke. Next level sriracha green juice, pour-over hashtag bushwick messenger bag blue bottle forage cold-pressed.</p>
+                <div className="mt-5 mb-3 archives">
+                    <h4 className="mb-3">Archives Placeholder</h4>
+                    <ul className="list-unstyled text-left">
+                        <li>2018</li>
+                        <li>2017</li>
+                        <li>2016</li>
+                    </ul>
+                </div>
+                <div className="mt-5 mb-3 destinations">
+                    <h4 className="mb-3">Destinations Placeholder</h4>
+                    <ul className="list-unstyled text-left">
+                        <li>North America</li>
+                        <li>South America</li>
+                        <li>Europe</li>
+                        <li>Africa</li>
+                        <li>Asia</li>
+                    </ul>
+                </div>
+                <div className="mt-5 mb-3 socials">
+                    <h4 className="mb-3">Connect</h4>
+                    <ul className="font-weight-bold list-inline d-flex justify-content-around">
+                        <li className="list-inline-item">I</li>
+                        <li className="list-inline-item">F</li>
+                        <li className="list-inline-item">T</li>
+                        <li className="list-inline-item">L</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- src/js/views/BlogPost.jsx
+++ src/js/views/BlogPost.jsx
@@ -13,11 +13,28 @@ export class BlogPost extends React.Component{
                 <Navbar />
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-12 col-md-9 postContentContainer">
-                            <h1 className="text-center postTitle">This is the Title of an Individual Blog Post</h1>
-                            <img src="http://via.placeholder.com/500x350" alt="post featured image" className="img-fluid"/>
-                            <p className="text-light datePublished">May 15, 2018</p>
-                            <p className="text-light author">Emily V.</p>
+                        <div className="col-12 col-md-9 border-right postContentContainer">
+                            <h1 className="text-center my-3 postTitle">This is the Title of an Individual Blog Post</h1>
+                            <div className="text-center postImageContainer">
+                                <img src="http://via.placeholder.com/800x500" alt="post featured image" className="img-fluid pb-3"/>
+                            </div>
+                            <p className="text-secondary datePublished">May 15, 2018</p>
+                            <p className="text-secondary author">Emily V.</p>
+                            <p className="postTextBlock">Lorem ipsum dolor amet flannel art party swag cardigan narwhal kinfolk vexillologist copper mug post ironic man bun neutra. Narwhal synth af portland ramps. Williamsburg blue bottle taiyaki narwhal seitan wolf, sustainable edison bulb kale chips occupy woke. Flannel seitan slow carb mustache ennui vaporware iceland pop up williamsburg shaman pour over gluten free heirloom. Pour over small batch farm to table, live edge you probably havent heard of them ethical XOXO four dollar toast fanny pack disrupt literally bicycle rights wolf jean shorts taiyaki.</p>
+                            <div className="text-center postImageContainer">
+                                <img src="http://via.placeholder.com/800x500" alt="post featured image" className="img-fluid pb-3"/>
+                            </div>
+                            <p className="postTextBlock">Lorem ipsum dolor amet flannel art party swag cardigan narwhal kinfolk vexillologist copper mug post ironic man bun neutra. Narwhal synth af portland ramps. Williamsburg blue bottle taiyaki narwhal seitan wolf, sustainable edison bulb kale chips occupy woke. Flannel seitan slow carb mustache ennui vaporware iceland pop up williamsburg shaman pour over gluten free heirloom. Pour over small batch farm to table, live edge you probably havent heard of them ethical XOXO four dollar toast fanny pack disrupt literally bicycle rights wolf jean shorts taiyaki.</p>
+                            <h3>This is a subtitle or section header</h3>
+                            <p className="postTextBlock">Lorem ipsum dolor amet flannel art party swag cardigan narwhal kinfolk vexillologist copper mug post ironic man bun neutra. Narwhal synth af portland ramps. Williamsburg blue bottle taiyaki narwhal seitan wolf, sustainable edison bulb kale chips occupy woke. Flannel seitan slow carb mustache ennui vaporware iceland pop up williamsburg shaman pour over gluten free heirloom. Pour over small batch farm to table, live edge you probably havent heard of them ethical XOXO four dollar toast fanny pack disrupt literally bicycle rights wolf jean shorts taiyaki.</p>
+                            <div className="row postImageContainer">
+                                <div className="col-md-6">
+                                    <img src="http://via.placeholder.com/500x350" alt="post featured image" className="img-fluid pb-3"/>
+                                </div>
+                                <div className="col-md-6">
+                                    <img src="http://via.placeholder.com/500x350" alt="post featured image" className="img-fluid pb-3"/>
+                                </div>
+                            </div>
                             <p className="postTextBlock">Lorem ipsum dolor amet flannel art party swag cardigan narwhal kinfolk vexillologist copper mug post ironic man bun neutra. Narwhal synth af portland ramps. Williamsburg blue bottle taiyaki narwhal seitan wolf, sustainable edison bulb kale chips occupy woke. Flannel seitan slow carb mustache ennui vaporware iceland pop up williamsburg shaman pour over gluten free heirloom. Pour over small batch farm to table, live edge you probably havent heard of them ethical XOXO four dollar toast fanny pack disrupt literally bicycle rights wolf jean shorts taiyaki.</p>
                         </div>
                         <div className="col">
