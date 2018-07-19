ed077eb9a2b5d8d835de55b40891c7fc22eccddc
KeShaunda <parrish309@gmail.com>
2018-07-19 21:55:27 +0000

    added shipping address inputs

--- src/js/component/BillingInfo.jsx
+++ src/js/component/BillingInfo.jsx
@@ -86,6 +86,78 @@ function BillingInfo(props){
                     <input type="checkbox" className="custom-control-input" id="save-info"></input>
                 </div>
                 <hr className="mb-4"></hr>
+                <h4 className="mb-3">Shipping Address</h4>
+
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" className="form-control" id="firstName" placeholder="" value="" required=""></input>
+                        <div className="invalid-feedback">
+                            Valid first name is required.
+                        </div>
+                    </div>
+                    
+                    <div className="col-md-6 mb-3">
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" className="form-control" id="lastName" placeholder="" value="" required=""></input>
+                        <div className="invalid-feedback">
+                            Valid last name is required.
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="mb-3">
+                    <label htmlFor="email">Email <span className="text-muted">(Optional)</span></label>
+                    <input type="email" className="form-control" id="email" placeholder="you@example.com"></input>
+                    <div className="invalid-feedback">
+                        Please enter a valid email address for shipping updates.
+                    </div>
+                </div>
+    
+                <div className="mb-3">
+                    <label htmlFor="address">Address</label>
+                    <input type="text" className="form-control" id="address" placeholder="1234 Main St" required=""></input>
+                    <div className="invalid-feedback">
+                        Please enter your shipping address.
+                    </div>
+                </div>
+                
+                <div className="mb-3">
+                    <label htmlFor="address2">Address 2 <span className="text-muted">(Optional)</span></label>
+                    <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"></input>
+                </div>
+    
+                <div className="row">
+                    <div className="col-md-5 mb-3">
+                        <label htmlFor="country">Country</label>
+                        <select className="custom-select d-block w-100" id="country" required="">
+                            <option value="">Choose...</option>
+                            <option>United States</option>
+                        </select>
+                        <div className="invalid-feedback">
+                          Please select a valid country.
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-3">
+                        <label htmlFor="state">State</label>
+                        <select className="custom-select d-block w-100" id="state" required="">
+                            <option value="">Choose...</option>
+                            <option>California</option>
+                        </select>
+                        <div className="invalid-feedback">
+                            Please provide a valid state.
+                        </div>
+                    </div>
+                    <div className="col-md-3 mb-3">
+                        <label htmlFor="zip">Zip</label>
+                        <input type="text" className="form-control" id="zip" placeholder="" required=""></input>
+                        <div className="invalid-feedback">
+                          Zip code required.
+                        </div>
+                    </div>
+                </div>
+                
+                <hr className="mb-4"></hr>
                 <h4 className="mb-3">Payment</h4>
                 
                 <div className="d-block my-3">
