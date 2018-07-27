2b58fd9375431619de6d9561fe25b4090a0704e7
Emily Vong <emily.vong25@gmail.com>
2018-07-27 23:58:51 +0000

    added link in navbar to cart

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import {Consumer} from "../stores/AppContext.jsx";//
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 
 class Navbar extends React.Component{
@@ -51,6 +52,7 @@ class Navbar extends React.Component{
                                   
                                     typeof state.session.username !== 'undefined' ?
                                         <div>    
+                                            <button className="btn btn-outline-light" ><Link to={"/shoppingcart/"} className="text-white"><FontAwesomeIcon icon={faShoppingCart}/> 0</Link></button>
                                             <button className="btn btn-outline-light" ><Link to={"/profile"} className="text-white"><FontAwesomeIcon icon={faUser}/> Profile</Link></button> {/*change 'Profile' to user's first name*/}
                                             <button className="btn btn-outline-light" ><Link to="#" className="text-white">Log Out</Link></button>
                                         </div>
