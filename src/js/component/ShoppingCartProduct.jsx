bff9ab2e7188475eefe38134588c064c08f6784e
KeShaunda <parrish309@gmail.com>
2018-07-30 02:33:51 +0000

    added remove button to shoppingcart

--- src/js/component/ShoppingCartProduct.jsx
+++ src/js/component/ShoppingCartProduct.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fastrash } from '@fortawesome/free-solid-svg-icons';
+
 
 import {Consumer} from "../stores/AppContext.jsx";
 
@@ -18,16 +21,15 @@ function ShoppingCartProduct(props){
                             <div className ="col-md-4">
                                 <h4 className ="product-name"><strong>{item.productName}</strong></h4><h4><small>{item.productDescription}</small></h4>
                             </div>
-                            <div className ="col-md-2">
-                                <div className ="col-sm-6 text-right">
-                                    <h6><strong>${item.productPrice} <span className="text-muted">x</span></strong></h6>
-                                </div>
+                            <div className ="col-md-4">
+                                {/*<div className ="col-sm-6 text-right">*/}
+                                <h6><strong>${item.productPrice} <span className="text-muted">x</span></strong></h6>
+                                {/*</div>*/}
                                 <div className ="col-sm-4">
                                     <input type="text" className="form-control input-sm" defaultValue="1" ></input>
                                 </div>
-                                <button type="button" className="btn btn-default btn-lg" aria-label="Right Align">
-                                    <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                                </button>
+                                {/*<button type="button" className="btn btn-default btn-lg" aria-label="Right Align"><FontAwesomeIcon icon="fastrash" /></button>*/}
+                                <button type="button" className="btn btn-outline-secondary">Remove</button>
                             </div>
                         </div>
                         <hr></hr>
