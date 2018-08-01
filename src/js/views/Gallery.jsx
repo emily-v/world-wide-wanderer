43d53c56fac9e229620c2d75e96292e22eeab3aa
Emily Vong <emily.vong25@gmail.com>
2018-08-01 15:11:17 +0000

    implemented gallery/single photo with dummy content

--- src/js/views/Gallery.jsx
+++ src/js/views/Gallery.jsx
@@ -6,39 +6,131 @@ import Navbar from '../component/Navbar.jsx';
 //import ProductCard from '../component/ProductCard.jsx';
 //import BlogSidebar from '../component/BlogSidebar.jsx';
 
-//import {Consumer} from "../stores/AppContext.jsx";
+import {Consumer} from "../stores/AppContext.jsx";
 
-export class Gallery extends React.Component{
+export class Gallery extends React.Component{ //--NEED TO FIX TAGS AND CLEAN UP CODE; ADD GALLERY TO NAVBAR--
     render(){
         return (
             <div>
                 <Navbar />
-                <div className="container-fluid">
-                    <div className="row no-gutters">
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/1200x1000" className="img-fluid img-thumbnail rounded-0" data-toggle="modal" data-target="#imagePopUp"/>
-                        </div>
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/600x500" className="img-fluid img-thumbnail rounded-0"/>
-                        </div>
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/600x500" className="img-fluid img-thumbnail rounded-0"/>
-                        </div>
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/600x500" className="img-fluid img-thumbnail rounded-0"/>
-                        </div>
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/600x500" className="img-fluid img-thumbnail rounded-0"/>
-                        </div>
-                        <div className="col-6 col-sm-4 col-lg-3">
-                            <img src="http://via.placeholder.com/600x500" className="img-fluid img-thumbnail rounded-0"/>
-                        </div>
-                        
-                        
-                    </div>
-                </div>
-                {/*-----START SINGLE IMAGE POP-UP MODAL-----*/}
-                <div className="modal fade" id="imagePopUp">
+                <Consumer>
+                    {({ state, actions }) =>
+                        (
+                            <React.Fragment>
+                                <div className="container-fluid">
+                                    <div className="row no-gutters">
+                                        {
+                                            state.photos.map((item,index)=>{
+                                                return(
+                                                    <div className="col-6 col-sm-4 col-lg-3" key={index}>
+                                                        <img src={item.photoURL} className="img-fluid img-thumbnail rounded-0" data-toggle="modal" data-target={"#imagePopUp"+item.photoID}/>
+                                                    </div>
+                                                );
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                                {/*-----START SINGLE IMAGE POP-UP MODAL-----*/}
+                                <div>
+                                    {
+                                        state.photos.map((item,index)=>{
+                                            return (
+                                                <div className="modal fade" id={"imagePopUp"+item.photoID} key={index}>
+                                                    <div className="modal-dialog modal-lg" role="document">
+                                                        <div className="modal-content">
+                                                            <button type="button" className="close text-right px-2" data-dismiss="modal">
+                                                                <span>&times;</span>
+                                                            </button>
+                                                            <div className="card">
+                                                                <img className="card-img-top" src={item.photoURL} />
+                                                                <div className="card-body py-2">
+                                                                    <div className="card-text">
+                                                                        <ul className="list-inline d-flex justify-content-between text-secondary">
+                                                                            <li className="list-inline-item">{item.location}</li>
+                                                                            <li className="list-inline-item">{item.dateTaken}</li>
+                                                                        </ul>
+                                                                        <p>{item.caption}</p>
+                                                                        <ul className="list-inline mb-0 text-center text-secondary">
+                                                                            <li className="list-inline-item px-sm-3">tag1</li>
+                                                                            <li className="list-inline-item px-sm-3">tag2</li>
+                                                                            <li className="list-inline-item px-sm-3">tag3</li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                                
+                                                                <a href="#" className="text-center mb-3" data-dismiss="modal"><small>close</small></a>
+                                                                
+                                                            </div>
+                                                            
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                            </React.Fragment>
+                        )
+                    }
+                </Consumer>       
+            </div>
+        );
+    }
+}
+
+/*Gallery.propTypes = {
+        photoID: PropTypes.number,
+        photoURL: PropTypes.string,
+        location: PropTypes.string,
+        dateTaken: PropTypes.string,
+        caption: PropTypes.string,
+        photoTags: PropTypes.string
+    };*/
+    
+    {/*<React.Fragment>
+                    {({ state, actions }) => 
+                        (
+                            state.photos.map((item,index)=>{
+                                return (
+                                    <div className="modal fade" id={"imagePopUp"+item.photoID} key={index}>
+                                        <div className="modal-dialog modal-lg" role="document">
+                                            <div className="modal-content">
+                                                <button type="button" className="close text-right px-2" data-dismiss="modal">
+                                                    <span>&times;</span>
+                                                </button>
+                                                <div className="card">
+                                                    <img className="card-img-top" src={item.photoURL} />
+                                                    <div className="card-body py-2">
+                                                        <div className="card-text">
+                                                            <ul className="list-inline d-flex justify-content-between text-secondary">
+                                                                <li className="list-inline-item">{item.location}</li>
+                                                                <li className="list-inline-item">{item.dateTaken}</li>
+                                                            </ul>
+                                                            <p>{item.caption}</p>
+                                                            <ul className="list-inline mb-0 text-center text-secondary">
+                                                                <li className="list-inline-item px-sm-3">tag1</li>
+                                                                <li className="list-inline-item px-sm-3">tag2</li>
+                                                                <li className="list-inline-item px-sm-3">tag3</li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    <a href="#" className="text-center mb-3" data-dismiss="modal"><small>close</small></a>
+                                                    
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        )
+                    }
+                </React.Fragment>*/}
+            
+                
+                
+                {/*<div className="modal fade" id="imagePopUp">
                     <div className="modal-dialog modal-lg" role="document">
                         <div className="modal-content">
                             <button type="button" className="close text-right px-2" data-dismiss="modal">
@@ -67,8 +159,4 @@ export class Gallery extends React.Component{
                             
                         </div>
                     </div>
-                </div>
-            </div>
-        );
-    }
-}
+                </div>*/}
